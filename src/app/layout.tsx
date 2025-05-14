a34c4af5e3edee6d40a9937c519fb15b52578935
AdaisukeV <daisuke-aoki@kke.co.jp>
2025-05-14 12:25:12 +0900

    Change the default theme from system to dark

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body className="bg-zinc-950">
           <ThemeProvider
             attribute="class"
-            defaultTheme="system"
+            defaultTheme="dark"
             enableSystem
             disableTransitionOnChange
           >
